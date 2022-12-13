866007a8618376d1c0a58fa3a5c7d6e0fb24b4f7
matthewmjm <matthewmjm@yahoo.com>
2022-12-13 10:29:55 -0600

    remove playground test js file for example tests

--- src/playground.js
+++ src/playground.js
@@ -1,14 +1,1 @@
-export const evenOrOdd = (num) => {
-  if (num % 2 === 0) {
-    return "even";
-  }
-  return "odd";
-};
 
-export const multiply = (num1, num2) => {
-  let total = 0;
-  for (let i = 0; i < num1; i++) {
-    total += num2;
-  }
-  return total;
-};
