52d8b722867dd41d0ed75a6fd9c9ab2c92631a8b
matthewmjm <matthewmjm@yahoo.com>
2022-12-14 15:27:59 -0600

    overwriting component data in a method

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,7 +1,6 @@
 <template>
   <button
     class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
-    v-on:click="handleClick"
   >
     Sign In
   </button>
@@ -10,10 +9,5 @@
 <script>
 export default {
   name: "ActionButton",
-  methods: {
-    handleClick() {
-      console.log("Button clicked");
-    },
-  },
 };
 </script>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -22,7 +22,7 @@
         </nav>
         <div class="ml-auto flex h-full items-center">
           <profile-image v-if="isLoggedIn" />
-          <action-button v-else />
+          <action-button v-else @click="loginUser" />
         </div>
       </div>
     </div>
@@ -52,6 +52,11 @@ export default {
       ],
       isLoggedIn: false,
     };
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true;
+    },
   },
 };
 </script>
