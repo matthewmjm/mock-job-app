c546759de458cfd35942de0f6b37952139c0a8c7
matthewmjm <matthewmjm@yahoo.com>
2022-12-14 09:57:26 -0600

    add Profile image component for avatar

---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="imageLink" class="h-8 w-8 rounded-3xl object-contain" />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png",
+    };
+  },
+};
+</script>
