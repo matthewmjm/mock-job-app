a66889039f30e19ba28bdc4f2980ea73106324d0
matthewmjm <matthewmjm@yahoo.com>
2022-12-15 15:33:07 -0600

    style subnav

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>This is the subnav</div>
+  <div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+    <div class="flex h-full items-center px-8">This is the Subnavigation</div>
+  </div>
 </template>
 
 <script>
