38b5cb0cd00c7d770c3afcb70a9b03114170e7af
matthewmjm <matthewmjm@yahoo.com>
2022-12-15 11:45:08 -0600

    computed property for button class object

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -22,7 +22,7 @@
         </nav>
         <div class="ml-auto flex h-full items-center">
           <profile-image v-if="isLoggedIn" />
-          <action-button v-else @click="loginUser" />
+          <action-button v-else @click="loginUser" :text="buttonText" />
         </div>
       </div>
     </div>
@@ -51,6 +51,7 @@ export default {
         "Jobs",
       ],
       isLoggedIn: false,
+      buttonText: "Log in",
     };
   },
   methods: {
