d8d036118719315a0393c25484edc21a55a0dc63
matthewmjm <matthewmjm@yahoo.com>
2022-12-16 13:30:07 -0600

    refactor tests with beforeEach and afterEach

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -1,33 +1,36 @@
+import { nextTick } from "vue";
 import { render, screen } from "@testing-library/vue";
 
 import TheHeadline from "@/components/TheHeadline.vue";
-import { nextTick } from "vue";
+import { vi } from "vitest";
 
 describe("TheHeadline", () => {
-  it("displays introductory action verb", () => {
+  beforeEach(() => {
     vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("displays introductory action verb", () => {
     render(TheHeadline);
 
     const actionPhrase = screen.getByRole("heading", {
       name: /build for everyone/i,
     });
     expect(actionPhrase).toBeInTheDocument();
-    vi.useRealTimers();
   });
 
   it("changes action verb at a specific interval", () => {
-    vi.useFakeTimers();
     const mock = vi.fn();
     vi.stubGlobal("setInterval", mock);
 
     render(TheHeadline);
 
     expect(mock).toHaveBeenCalled();
-    vi.useRealTimers();
   });
 
   it("swaps action verb after interval", async () => {
-    vi.useFakeTimers();
     render(TheHeadline);
     vi.advanceTimersToNextTimer();
 
@@ -40,7 +43,6 @@ describe("TheHeadline", () => {
   });
 
   it("removes interval when component disappears", () => {
-    vi.useFakeTimers();
     const clearInterval = vi.fn();
     vi.stubGlobal("clearInterval", clearInterval);
 
@@ -48,6 +50,5 @@ describe("TheHeadline", () => {
     unmount();
 
     expect(clearInterval).toHaveBeenCalled();
-    vi.useRealTimers();
   });
 });
