45a63e4014238622f7737842eefce3ce1fe81fe1
matthewmjm <matthewmjm@yahoo.com>
2022-12-16 10:22:39 -0600

    introduce css grid to hero section

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,6 +1,21 @@
 <template>
   <main>
-    <section>The Hero Component</section>
+    <section class="flex h-screen flex-col bg-yellow-200 pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-span-1 col-start-1 border border-blue-700">
+          1 Column
+        </div>
+        <div class="col-span-5 col-start-2 border border-blue-700">
+          5 Columns
+        </div>
+        <div class="col-span-5 col-start-7 border border-blue-700">
+          5 Columns
+        </div>
+        <div class="col-span-1 col-start-12 border border-blue-700">
+          1 Column
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
