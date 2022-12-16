c2f6a2a670bc061549be640088d58571f6762432
matthewmjm <matthewmjm@yahoo.com>
2022-12-16 13:23:33 -0600

    write more tests for display next action verb, changes action verb, swaps action verb after interval, and removes interval when unmount

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -1,13 +1,53 @@
 import { render, screen } from "@testing-library/vue";
 
 import TheHeadline from "@/components/TheHeadline.vue";
+import { nextTick } from "vue";
 
 describe("TheHeadline", () => {
-  describe("Vitest playground", () => {
-    it("tracks whether it has been called", () => {
-      const mockFunction = vi.fn();
-      mockFunction(1, 2);
-      expect(mockFunction).toHaveBeenCalledWith(1, 2);
+  it("displays introductory action verb", () => {
+    vi.useFakeTimers();
+    render(TheHeadline);
+
+    const actionPhrase = screen.getByRole("heading", {
+      name: /build for everyone/i,
     });
+    expect(actionPhrase).toBeInTheDocument();
+    vi.useRealTimers();
+  });
+
+  it("changes action verb at a specific interval", () => {
+    vi.useFakeTimers();
+    const mock = vi.fn();
+    vi.stubGlobal("setInterval", mock);
+
+    render(TheHeadline);
+
+    expect(mock).toHaveBeenCalled();
+    vi.useRealTimers();
+  });
+
+  it("swaps action verb after interval", async () => {
+    vi.useFakeTimers();
+    render(TheHeadline);
+    vi.advanceTimersToNextTimer();
+
+    await nextTick();
+    const actionPhrase = screen.getByRole("heading", {
+      name: /create for everyone/i,
+    });
+
+    expect(actionPhrase).toBeInTheDocument();
+  });
+
+  it("removes interval when component disappears", () => {
+    vi.useFakeTimers();
+    const clearInterval = vi.fn();
+    vi.stubGlobal("clearInterval", clearInterval);
+
+    const { unmount } = render(TheHeadline);
+    unmount();
+
+    expect(clearInterval).toHaveBeenCalled();
+    vi.useRealTimers();
   });
 });
